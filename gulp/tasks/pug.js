511ce15178196056b53062a4a6f467a449cd8b2d
Dmitriy <CheChir.DH@gmail.com>
2019-01-08 11:57:34 +0300

    Плумбер не ловил ошибку. Галп крашился после ошибки в синтаксисе pug файла. (#1)
    Теперь в консоль идет ошибка и галп прожолжает слеить за файлами.

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -9,16 +9,17 @@ let plumber = require('gulp-plumber'),
 module.exports = function () {
     $.gulp.task('pug', () => {
         return $.gulp.src('./dev/pug/*.pug')
+            .pipe(plumber())
             .pipe(changed('dist', {extension: '.html'}))
             .pipe(gulpif(global.isWatching, cached('pug')))
             .pipe(pugInheritance({basedir: './dev/pug/', skip: 'node_modules'}))
             .pipe(filter(function (file) {
                 return !/\/_/.test(file.path) && !/^_/.test(file.relative);
             }))
-            .pipe(plumber())
             .pipe(pug({
                 pretty: true
             }))
+            .pipe(plumber.stop())
             .pipe($.gulp.dest('./build/'))
             .on('end', $.browserSync.reload);
     });
