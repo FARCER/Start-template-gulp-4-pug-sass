704baa4fa9289e953c3e7596ec078bf05ceb0ee8
Anatoliy Salamatin <counter18@yandex.ru>
2019-09-07 22:41:51 +0300

    gulp-changed (Destination directory) #9 https://github.com/FARCER/Start-template-gulp-4-pug-sass/issues/9
    (cherry picked from commit ae5e189555fa051daad1151cd8368a82f1ad9b16)

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,13 +1,25 @@
-const plumber = require('gulp-plumber'),
-      pug = require('gulp-pug');
+let plumber = require('gulp-plumber'),
+    pug = require('gulp-pug'),
+    pugInheritance = require('gulp-pug-inheritance'),
+    changed = require('gulp-changed'),
+    cached = require('gulp-cached'),
+    gulpif = require('gulp-if'),
+    filter = require('gulp-filter');
 
 module.exports = function () {
     $.gulp.task('pug', () => {
         return $.gulp.src('./dev/pug/*.pug')
             .pipe(plumber())
+            .pipe(changed('build', {extension: '.html'}))
+            .pipe(gulpif(global.isWatching, cached('pug')))
+            .pipe(pugInheritance({basedir: './dev/pug/', skip: 'node_modules'}))
+            .pipe(filter(function (file) {
+                return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+            }))
             .pipe(pug({
                 pretty: true
             }))
+            .pipe(plumber.stop())
             .pipe($.gulp.dest('./build/'))
             .on('end', $.browserSync.reload);
     });
